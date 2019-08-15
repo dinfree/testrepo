99d5bc9f33bd5cd81a299e86763c61cb80b21336
fax20 <53804011+fax20@users.noreply.github.com>
2019-08-16 01:21:31 +0900

    Delete IClassroom.java

--- 201533844이호정/IClassroom.java
+++
@@ -1,9 +0,0 @@
-package javapj;
-
-
-public interface IClassroom { //MyClassroom에서 사용할 인터페이스
-     	public abstract void setTitle(String title);
-		public abstract void genUser();
-		public abstract void genRoom();
-		public abstract void start();
-}
