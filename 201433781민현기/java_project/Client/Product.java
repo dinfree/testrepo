58588818b4bee70f2e15924900f1f7a77e3114c3
HyeonGi Min <hyeoni1995@gmail.com>
2019-08-16 14:18:22 +0900

    201433781 민현기 자바 과제 제출

--- 201433781민현기/java_project/Client/Product.java
+++ 201433781민현기/java_project/Client/Product.java
@@ -8,8 +8,8 @@ public abstract class Product {
 
     // 상품명과 가격을 출력하는 기본정보 출력 메서드
     public void printDetail() {
-        System.out.printf("상품명:%s, 가격:%d, ",pname,price);
-        printExtra();
+        System.out.printf("상품명:%s, 가격:%d \n",pname,price);
+      //  printExtra();
     }
 
     public abstract void printExtra();
