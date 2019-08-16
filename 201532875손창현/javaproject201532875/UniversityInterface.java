87216c1c76a22345bc47bf1888850eb35f27cf17
changhyunSoun <53833541+changhyunSoun@users.noreply.github.com>
2019-08-16 18:44:19 +0900

    Delete UniversityInterface.java

--- 201532875손창현/javaproject201532875/UniversityInterface.java
+++
@@ -1,9 +0,0 @@
-package javaproject201532875;
-
-public interface UniversityInterface {
-	void setTitle(String title);
-	void genStudent();
-	void genStudentInfo();
-	void genLectureInfo();
-	void launch();
-}
